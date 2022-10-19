c0973b34d8c7295ec841c99b8e81597727535bc1
Angus-Morton <angus.morton@phs.scot>
2022-10-19 11:05:43 +0100

    Put in step headings

--- code/placeholder.R
+++ code/placeholder.R
@@ -1,3 +1,43 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~
 # 1_processing_for_KPI_11-13.R
+# Angus Morton
+# 19/10/2022
+#
+# Create the invite/uptake file used for producing KPI 1.1-1.3
+#
+# Written on RServer (R Version 3.6.1)
 #~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# invite/uptake file is a patient level extract with information on invites
+# and attendances for each individual
+
+# RDS file path
+"\\Isdsf00d03\AAA\Topics\Screening\extracts\202209\output"
+
+# Step 1
+# Step 2 
+
+# Step 1: Create derived variables
+
+# Step 2: Create a first offer sent file
+
+# STEP 3: Create first screening result file
+
+# STEP 4: Create baseline cohorts file
+
+# STEP 5: Create exclusions files
+
+*STEP 6: MATCH THE COHORT, FIRST OFFER, FIRST RESULT AND EXCLUSION FILES
+THEN APPLY EXCLUSION CRITERIA.
+
+
+
+
+
+### Step 1 ----
+
+### Step 2 ----
+
+### Step 3 ----
+
+
