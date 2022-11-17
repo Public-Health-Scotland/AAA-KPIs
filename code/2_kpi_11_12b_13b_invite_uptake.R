f77b7b71b798644362b15a8d56c5c6054073afb2
Angus-Morton <angus.morton@phs.scot>
2022-11-17 13:05:58 +0000

    Created R script 2

---
+++ code/2_kpi_11_12b_13b_invite_uptake.R
@@ -0,0 +1,10 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2_kpi_11_12b_13b_invite_uptake.R
+# Angus Morton
+# 17/11/2022
+#
+# Produce KPIs 1.1, 1.2b and 1.3b
+#
+# Written on RServer (R Version 3.6.1)
+#~~~~~~~~~~~~~~~~~~~~~~~~~
+
