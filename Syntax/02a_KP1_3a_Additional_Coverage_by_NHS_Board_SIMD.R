50a240f0c2be505e1baf2b9b759abd6376efcb96
eibhlin-os <eibhlin.osullivan@phs.scot>
2023-01-12 15:03:35 +0000

    file set up

---
+++ Syntax/02a_KP1_3a_Additional_Coverage_by_NHS_Board_SIMD.R
@@ -0,0 +1,13 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 02a_KP1_3a_Additional_Coverage_by_NHS_Board_SIMD.R
+# Eibhlin O'Sullivan
+# Jan 2022
+# Takes processed extract and creates summary of patients
+# Written/run on R Studio Server
+# R version 3.6.1
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+### 1 - Housekeeping ----
+
+## Source housekeeping file
+source(here::here("Syntax", "00_housekeeping.R"))
