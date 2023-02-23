a6c2d6b35e76ab533fb8612d46b12ee7080b6861
Angus-Morton <angus.morton@phs.scot>
2023-02-23 09:43:10 +0000

    got all the data in

--- code/x_coverage_gp_practice.R
+++ code/x_coverage_gp_practice.R
@@ -36,6 +36,11 @@ gp_prac_b_fpath <- paste0("/PHI_conf/AAA/Topics/Screening/KPI/202209/data/",
 gp_lookup_fpath <- paste0("/conf/linkage/output/lookups/Unicode/",
                           "National Reference Files/gpprac.sav")
 
+prev_gp_data_fpath <- paste0("/PHI_conf/AAA/Topics/Screening/publications/",
+                             "Completed/20220301/Temp/Management Information/",
+                             "Practice/",
+                             "Temp All Boards output_coverage 2021.zsav")
+
 
 year1_start <- dmy("01-04-1955")
 year1_end <- dmy("31-03-1956")
@@ -46,14 +51,19 @@ year2_end <- dmy("31-03-1957")
 
 ### Step 2 : Import data ----
 
-invite_uptake <- read_rds(invite_uptake_fpath)
+#invite_uptake <- read_rds(invite_uptake_fpath)
 coverage_basefile <- read_rds(paste0(output_fpath,
                                      "coverage_basefile.rds"))
 
 gp_prac_a <- read_csv(gp_prac_a_fpath)
 gp_prac_b <- read_csv(gp_prac_b_fpath)
 
-gp_lookup <- read_sav(gp_lookup_fpath)
+gp_lookup <- read_sav(gp_lookup_fpath) %>%
+  select(praccode, add1)
+
+#prev_gp_data <- read_sav(prev_gp_data_fpath)
+
+prev_gp_data <- read_rds(prev_gp_data_fpath)
 
 ### Step 3 : ----
 
@@ -77,7 +87,7 @@ coverage_gp <- coverage_basefile %>%
   rename(practice_code = `Practice Code`)
 
 ### Assign gp practices to health boards
-# The spss script has a big gp practice cypher
+# The spss script has a big gp practice if/else statement
 # !! come back to check if this is the right thing
 # creates 'cypher' doesn't seem to be used
 # creates 'prac2' which is practice code without the leading HB letter
@@ -124,9 +134,86 @@ coverage_gp <- coverage_gp %>%
     hbres == "Western Isles" & between(gp_join, 9000, 9099) ~ 1,
     hbres == "Dumfries & Galloway" & between(gp_join, 1800, 1999) ~ 1,
     hbres == "Shetland" & between(gp_join, 3900, 3999) ~ 1,
+    is.na(gp_join) ~ 1,
     TRUE ~ 0
   ))
 
-# Add in practice description using reference lookup file
+# create a variable for practice code if registered in health board
+coverage_gp <- coverage_gp %>%
+  mutate(gp_hb = case_when(
+    in_hb == 0 ~ "Practice outside hb area",
+    TRUE ~ gp_join
+  ))
 
-# calculate coverage KPIs
+# Add in practice description using reference lookup file
+gp_lookup <- gp_lookup %>%
+  rename(gp_join = praccode,
+         gp_desc = add1) %>%
+  mutate(gp_join = substr(gp_join, 1, 4))
+
+# There are two '9999' codes (99995=practice in england, wales, ni)
+# and (99999=unknown). Take 9999 as unknown.
+gp_lookup <- gp_lookup %>%
+  filter(!(gp_join == 9999 & gp_desc == "PATIENTS REGISTERED WITH A GP"))
+
+coverage_gp <- coverage_gp %>%
+  left_join(gp_lookup, by = "gp_join")
+
+coverage_gp <- coverage_gp %>%
+  mutate(gp_desc = if_else(gp_hb == "Practice outside hb area",
+                           "Practice outside hb area", gp_desc))
+
+
+
+# calculate KPI 1.2a (coverage)
+
+breakdown_1_2a <- coverage_gp %>%
+  group_by(gp_hb, gp_desc, hbres) %>%
+  summarise(
+    across(cohort_year1:tested2_any_not_assigned, sum, na.rm = TRUE)
+  ) %>%
+  ungroup() %>%
+  mutate(gp_hb = if_else(is.na(gp_hb), "Unknown Practice", gp_hb))
+
+
+hb_1_2a <- coverage_gp %>%
+  group_by(hbres) %>%
+  summarise(
+    across(cohort_year1:tested2_any_not_assigned, sum, na.rm = TRUE)
+  ) %>%
+  ungroup() %>%
+  mutate(gp_hb = hbres)
+
+breakdown_1_2a <- bind_rows(breakdown_1_2a, hb_1_2a)
+
+# create percentages
+breakdown_1_2a <- breakdown_1_2a %>%
+  mutate(
+    percent_year2 = (tested2_year2/cohort_year2)*100,
+    
+    percent_any_year2 = (tested2_any_year2/cohort_year2)*100
+  )
+
+# Output tables
+output_1_2a <- breakdown_1_2a %>%
+  select(hbres, gp_hb, gp_desc,
+         cohort_year2, tested2_year2, percent_year2) %>%
+  arrange(hbres, gp_hb)
+
+
+# bring in previous year's data
+# update practice names using most recent lookup to avoid duplication
+
+prev_gp_data <- prev_gp_data %>%
+  left_join(gp_lookup, by = c("prac3" = "gp_join"))
+
+# now join to most recent year
+output_2year <- output_1_2a %>%
+  full_join(prev_gp_data, by = c("hbres" = "hbresname",
+                                 "gp_hb" = "prac3",
+                                 "gp_desc" = "gp_desc")) %>%
+  select(hbres, gp_hb, gp_desc, FY2020_21_cohort, FY2020_21_tested, p_FY2020_21,
+         cohort_year2, tested2_year2, percent_year2)
+
+
+# save out
