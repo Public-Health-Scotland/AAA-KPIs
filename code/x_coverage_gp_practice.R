ade60bb55b4f9cd0b232db3fc4bbe1cd26897023
Angus-Morton <angus.morton@phs.scot>
2023-03-24 17:12:34 +0000

    Bit more tidying

--- code/x_coverage_gp_practice.R
+++ code/x_coverage_gp_practice.R
@@ -17,6 +17,77 @@ library(tidylog)
 library(lubridate)
 library(openxlsx)
 
+# Function
+
+# The spss script has a big gp practice if/else statement
+# !! come back to check if this is the right thing
+# creates 'cypher' doesn't seem to be used
+# creates 'prac2' which is practice code without the leading HB letter
+# prac2 is used for joining on the reference file
+# creates 'prac3' which is prac2 unless patient is resident outside the
+# health board they're registered in
+# prac3 is used for aggregation
+
+make_gp_vars <- function(df, gp_lookup) {
+  
+  
+  # flag for if an individual is registered inside/outside their hb of residence
+  dat <- df %>%
+    mutate(in_hb = case_when(
+      gp_join == 3139 ~ 1,
+      hbres == "Ayrshire & Arran" & between(gp_join, 8000, 8399) ~ 1,
+      hbres == "Borders" & between(gp_join, 1600, 1799) ~ 1,
+      hbres == "Fife" & between(gp_join, 2000, 2499) ~ 1,
+      hbres == "Lanarkshire" & 
+        (between(gp_join, 6000, 6599) |
+           gp_join %in% c(4626,4627,4653,4654,
+                          4900,4902,4905,4906,
+                          4911,4925,4943,4952,
+                          4964,4969,4970,4979)) ~ 1,
+      hbres == "Greater Glasgow & Clyde" &
+        (between(gp_join, 4000, 5499) |
+           between(gp_join, 8499, 8799)) &
+        !gp_join %in% c(4626,4627,4653,4654,
+                        4900,4902,4905,4906,
+                        4911,4925,4943,4952,
+                        4964,4969,4970,4979,
+                        8500,8511,8514,8515,
+                        8519) ~ 1,
+      hbres == "Highland" &
+        (between(gp_join, 5500, 5999) |
+           between(gp_join, 8400, 8498) |
+           gp_join %in% c(8500,8511,8514,8515,8519)) ~ 1,
+      hbres == "Grampian" & between(gp_join, 3000, 3799) ~ 1,
+      hbres == "Lothian" & between(gp_join, 7000, 7999) ~ 1,
+      hbres == "Orkney" & between(gp_join, 3800, 3899) ~ 1,
+      hbres == "Tayside" & between(gp_join, 1000, 1599) ~ 1,
+      hbres == "Forth Valley" & between(gp_join, 2500, 2999) ~ 1,
+      hbres == "Western Isles" & between(gp_join, 9000, 9099) ~ 1,
+      hbres == "Dumfries & Galloway" & between(gp_join, 1800, 1999) ~ 1,
+      hbres == "Shetland" & between(gp_join, 3900, 3999) ~ 1,
+      is.na(gp_join) ~ 1,
+      TRUE ~ 0
+    ))
+  
+  # create a variable for practice code if registered in health board
+  dat <- dat %>%
+    mutate(gp_hb = case_when(
+      in_hb == 0 ~ "Practice outside hb area",
+      TRUE ~ gp_join
+    ))
+  
+  # Join on gp lookup to get names
+  dat <- dat %>%
+    left_join(gp_lookup, by = "gp_join")
+  
+  # Create gp description for output
+  dat <- dat %>%
+    mutate(gp_desc = if_else(gp_hb == "Practice outside hb area",
+                             "Practice outside hb area", gp_desc))
+  
+  dat
+  
+}
 
 # This should be the only step which needs edited each time
 
@@ -26,6 +97,11 @@ invite_uptake_fpath <- paste0("/PHI_conf/AAA/Topics/Screening/KPI/202209/",
 
 output_fpath <- paste0("/PHI_conf/AAA/Topics/Screening/KPI/202209/",
                        "temp/KPIs/KPI1.1 - KPI1.3/")
+
+# AAA extract (needed for self referral data)
+extract_fpath <- paste0("/PHI_conf/AAA/Topics/Screening/extracts/",
+                        "202209/output/aaa_extract_202209.rds")
+
 
 gp_prac_a_fpath <- paste0("/PHI_conf/AAA/Topics/Screening/KPI/202209/data/",
                           "GP Practice History with dob selection - prior to 1_4_1952.csv")
@@ -54,19 +130,22 @@ year2_end <- dmy("31-03-1957")
 
 ### Step 2 : Import data ----
 
-#invite_uptake <- read_rds(invite_uptake_fpath)
 coverage_basefile <- read_rds(paste0(output_fpath,
                                      "coverage_basefile.rds"))
 
 gp_prac_a <- read_csv(gp_prac_a_fpath)
 gp_prac_b <- read_csv(gp_prac_b_fpath)
 
+
 gp_lookup <- read_sav(gp_lookup_fpath) %>%
-  select(praccode, add1)
+  select(gp_join = praccode,
+         gp_desc = add1) %>%
+  mutate(gp_join = substr(gp_join, 1, 4)) %>%
+  filter(!(gp_join == 9999 & gp_desc == "PATIENTS REGISTERED WITH A GP"))
 
 prev_gp_data <- read_rds(prev_gp_data_fpath)
 
-### Step 3 : ----
+### Step 3 : Add GP registration episodes ----
 
 gp_prac <- bind_rows(gp_prac_a, gp_prac_b)
 
@@ -87,87 +166,17 @@ coverage_gp <- coverage_basefile %>%
   select(-c(`Area of Residence`, `Valid from`, `Valid to`, `valid`)) %>%
   rename(practice_code = `Practice Code`)
 
-### Assign gp practices to health boards
-# The spss script has a big gp practice if/else statement
-# !! come back to check if this is the right thing
-# creates 'cypher' doesn't seem to be used
-# creates 'prac2' which is practice code without the leading HB letter
-# prac2 is used for joining on the reference file
-# creates 'prac3' which is prac2 unless patient is resident outside the
-# health board they're registered in
-# prac3 is used for aggregation
+### Step 4 : Assign gp practices to health boards ---
 
 # make gp_join
 coverage_gp <- coverage_gp %>%
   mutate(gp_join = substr(practice_code, 2, 5))
 
-# flag for if an individual is registered inside/outside their hb of residence
-coverage_gp <- coverage_gp %>%
-  mutate(in_hb = case_when(
-    gp_join == 3139 ~ 1,
-    hbres == "Ayrshire & Arran" & between(gp_join, 8000, 8399) ~ 1,
-    hbres == "Borders" & between(gp_join, 1600, 1799) ~ 1,
-    hbres == "Fife" & between(gp_join, 2000, 2499) ~ 1,
-    hbres == "Lanarkshire" & 
-      (between(gp_join, 6000, 6599) |
-      gp_join %in% c(4626,4627,4653,4654,
-                     4900,4902,4905,4906,
-                     4911,4925,4943,4952,
-                     4964,4969,4970,4979)) ~ 1,
-    hbres == "Greater Glasgow & Clyde" &
-      (between(gp_join, 4000, 5499) |
-         between(gp_join, 8499, 8799)) &
-         !gp_join %in% c(4626,4627,4653,4654,
-                      4900,4902,4905,4906,
-                      4911,4925,4943,4952,
-                      4964,4969,4970,4979,
-                      8500,8511,8514,8515,
-                      8519) ~ 1,
-    hbres == "Highland" &
-      (between(gp_join, 5500, 5999) |
-         between(gp_join, 8400, 8498) |
-         gp_join %in% c(8500,8511,8514,8515,8519)) ~ 1,
-    hbres == "Grampian" & between(gp_join, 3000, 3799) ~ 1,
-    hbres == "Lothian" & between(gp_join, 7000, 7999) ~ 1,
-    hbres == "Orkney" & between(gp_join, 3800, 3899) ~ 1,
-    hbres == "Tayside" & between(gp_join, 1000, 1599) ~ 1,
-    hbres == "Forth Valley" & between(gp_join, 2500, 2999) ~ 1,
-    hbres == "Western Isles" & between(gp_join, 9000, 9099) ~ 1,
-    hbres == "Dumfries & Galloway" & between(gp_join, 1800, 1999) ~ 1,
-    hbres == "Shetland" & between(gp_join, 3900, 3999) ~ 1,
-    is.na(gp_join) ~ 1,
-    TRUE ~ 0
-  ))
+coverage_gp <- make_gp_vars(coverage_gp, gp_lookup)
 
-# create a variable for practice code if registered in health board
-coverage_gp <- coverage_gp %>%
-  mutate(gp_hb = case_when(
-    in_hb == 0 ~ "Practice outside hb area",
-    TRUE ~ gp_join
-  ))
-
-# Add in practice description using reference lookup file
-gp_lookup <- gp_lookup %>%
-  rename(gp_join = praccode,
-         gp_desc = add1) %>%
-  mutate(gp_join = substr(gp_join, 1, 4))
-
-# There are two '9999' codes (99995=practice in england, wales, ni)
-# and (99999=unknown). Take 9999 as unknown.
-gp_lookup <- gp_lookup %>%
-  filter(!(gp_join == 9999 & gp_desc == "PATIENTS REGISTERED WITH A GP"))
-
-coverage_gp <- coverage_gp %>%
-  left_join(gp_lookup, by = "gp_join")
-
-coverage_gp <- coverage_gp %>%
-  mutate(gp_desc = if_else(gp_hb == "Practice outside hb area",
-                           "Practice outside hb area", gp_desc))
-
-
+### Step x : Calculate KPI 1.2a by GP ----
 
 # calculate KPI 1.2a (coverage)
-
 breakdown_1_2a <- coverage_gp %>%
   group_by(gp_hb, gp_desc, hbres) %>%
   summarise(
@@ -175,7 +184,6 @@ breakdown_1_2a <- coverage_gp %>%
   ) %>%
   ungroup() %>%
   mutate(gp_hb = if_else(is.na(gp_hb), "Unknown Practice", gp_hb))
-
 
 hb_1_2a <- coverage_gp %>%
   group_by(hbres) %>%
@@ -201,14 +209,15 @@ output_1_2a <- breakdown_1_2a %>%
          cohort_year1, tested2_year1, percent_year1) %>%
   arrange(hbres, gp_hb)
 
-# output for next year
+## Save output for next year
 write_rds(output_1_2a, paste0(gp_output_fpath,
                               "All Boards output_coverage 2122.rds"))
 
 
+### Step x : Create KPI 1.2a GP output ----
+
 # bring in previous year's data
 # update practice names using most recent lookup to avoid duplication
-
 prev_gp_data <- prev_gp_data %>%
   left_join(gp_lookup, by = c("prac3" = "gp_join")) %>%
   mutate(prac3 = case_when(
@@ -225,7 +234,6 @@ output_2year <- output_1_2a %>%
   select(hbres, gp_hb, gp_desc, FY2020_21_cohort, FY2020_21_tested, p_FY2020_21,
          cohort_year1, tested2_year1, percent_year1) %>%
   arrange(hbres, gp_hb)
-
 
 # save out
 # Format to be like the old file. This step can be taken out once the macro
@@ -254,11 +262,7 @@ writeData(wb, sheet = "data", output_2year)
 saveWorkbook(wb, paste0(output_fpath, "gp_practice_all_boards.xlsx"), overwrite = TRUE)
 
 
-### Self referrals
-
-# bring in AAA extract
-extract_fpath <- paste0("/PHI_conf/AAA/Topics/Screening/extracts/",
-                        "202209/output/aaa_extract_202209.rds")
+### Step x : Self referrals ----
 
 extract <- read_rds(extract_fpath)
 
@@ -281,57 +285,7 @@ extract_gp <- extract_slim %>%
 extract_gp <- extract_gp %>%
   mutate(gp_join = substr(practice_code, 2, 5))
 
-# do all the practice to health board stuff
-extract_gp <- extract_gp %>%
-  mutate(in_hb = case_when(
-    gp_join == 3139 ~ 1,
-    hbres == "Ayrshire & Arran" & between(gp_join, 8000, 8399) ~ 1,
-    hbres == "Borders" & between(gp_join, 1600, 1799) ~ 1,
-    hbres == "Fife" & between(gp_join, 2000, 2499) ~ 1,
-    hbres == "Lanarkshire" & 
-      (between(gp_join, 6000, 6599) |
-         gp_join %in% c(4626,4627,4653,4654,
-                        4900,4902,4905,4906,
-                        4911,4925,4943,4952,
-                        4964,4969,4970,4979)) ~ 1,
-    hbres == "Greater Glasgow & Clyde" &
-      (between(gp_join, 4000, 5499) |
-         between(gp_join, 8499, 8799)) &
-      !gp_join %in% c(4626,4627,4653,4654,
-                      4900,4902,4905,4906,
-                      4911,4925,4943,4952,
-                      4964,4969,4970,4979,
-                      8500,8511,8514,8515,
-                      8519) ~ 1,
-    hbres == "Highland" &
-      (between(gp_join, 5500, 5999) |
-         between(gp_join, 8400, 8498) |
-         gp_join %in% c(8500,8511,8514,8515,8519)) ~ 1,
-    hbres == "Grampian" & between(gp_join, 3000, 3799) ~ 1,
-    hbres == "Lothian" & between(gp_join, 7000, 7999) ~ 1,
-    hbres == "Orkney" & between(gp_join, 3800, 3899) ~ 1,
-    hbres == "Tayside" & between(gp_join, 1000, 1599) ~ 1,
-    hbres == "Forth Valley" & between(gp_join, 2500, 2999) ~ 1,
-    hbres == "Western Isles" & between(gp_join, 9000, 9099) ~ 1,
-    hbres == "Dumfries & Galloway" & between(gp_join, 1800, 1999) ~ 1,
-    hbres == "Shetland" & between(gp_join, 3900, 3999) ~ 1,
-    is.na(gp_join) ~ 1,
-    TRUE ~ 0
-  ))
-
-# create a variable for practice code if registered in health board
-extract_gp <- extract_gp %>%
-  mutate(gp_hb = case_when(
-    in_hb == 0 ~ "Practice outside hb area",
-    TRUE ~ gp_join
-  ))
-
-extract_gp <- extract_gp %>%
-  left_join(gp_lookup, by = "gp_join")
-
-extract_gp <- extract_gp %>%
-  mutate(gp_desc = if_else(gp_hb == "Practice outside hb area",
-                           "Practice outside hb area", gp_desc))
+extract_gp <- make_gp_vars(extract_gp, gp_lookup)
 
 # aggregate to get totals
 # need totals by health board
@@ -370,7 +324,3 @@ wb <- createWorkbook()
 addWorksheet(wb, "data")
 writeData(wb, sheet = "data", self_ref_gp)
 saveWorkbook(wb, paste0(output_fpath, "sr_all_boards.xlsx"), overwrite = TRUE)
-
-
-
-
