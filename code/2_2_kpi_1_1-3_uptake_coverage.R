8760bf62b4dc6bc76eae74e6680f93d515198863
aoifem01 <aoife.mccarthy@phs.scot>
2024-03-21 16:53:55 +0000

    Adding KPI 1.1 Scotland-level SIMD (change not yet reflected in downstream scripts).

--- code/2_2_kpi_1_1-3_uptake_coverage.R
+++ code/2_2_kpi_1_1-3_uptake_coverage.R
@@ -311,6 +311,60 @@ kpi_1_1 <- kpi_1_1 |>
 
 kpi_1_1 <- hb_tibble |> left_join(kpi_1_1, by = "hbres")
 
+## KPI 1.1 Scotland SIMD ----
+# KPI 1.1: individuals offered screen before 66 (+ additional COVID info)
+# grouped by Scotland-level SIMD quintiles
+kpi_1_1_simd <- invite_uptake  |> 
+  select(hbres, simd2020v2_sc_quintile, cohort_year1:offer_add_year2) |> 
+  group_by(hbres, simd2020v2_sc_quintile) |> 
+  summarise(across(cohort_year1:offer_add_year2, sum, na.rm = TRUE)) |> 
+  group_modify(~ janitor::adorn_totals(.x, where = "row", name = "Total")) |>  
+  ungroup() |> 
+  glimpse() # _not_assigned: what do these rows do??
+
+# Scotland
+kpi_1_1_simd_scot <- invite_uptake  |> 
+  select(simd2020v2_sc_quintile, cohort_year1:offer_add_year2) |> 
+  group_by(simd2020v2_sc_quintile) |> 
+  summarise(across(cohort_year1:offer_add_year2, sum, na.rm = TRUE)) |> 
+  group_modify(~ janitor::adorn_totals(.x, where = "row", name = "Total")) |>  
+  ungroup() |> 
+  mutate(hbres = "Scotland", .before = simd2020v2_sc_quintile) |> 
+  glimpse()
+
+# Combine & order by SIMD
+kpi_1_1_simd <- bind_rows(kpi_1_1_simd_scot, kpi_1_1_simd) |> 
+  mutate(simd2020v2_sc_quintile = if_else(is.na(simd2020v2_sc_quintile), 
+                                          "Unknown", simd2020v2_sc_quintile))
+kpi_1_1_simd <- simd_level |> left_join(kpi_1_1_simd, 
+                                        by = c("simd" = "simd2020v2_sc_quintile"))
+
+rm(kpi_1_1_simd_scot) # tidy environment
+
+kpi_1_1_simd <- kpi_1_1_simd  |> 
+  mutate(coverage_year1 = (offer_year1/cohort_year1)*100,
+         coverage_year2 = (offer_year2/cohort_year2)*100,
+         coverage_add_year1 = (offer_add_year1/cohort_year1)*100,
+         coverage_add_year2 = (offer_add_year2/cohort_year2)*100) |> 
+  select(hbres, simd, cohort_year1, offer_year1, coverage_year1, offer_add_year1,
+         coverage_add_year1, cohort_year2, offer_year2, coverage_year2,
+         offer_add_year2, coverage_add_year2) # these last 2 only used in fall MEG
+
+# Reformat to match historical data
+kpi_1_1_simd <- kpi_1_1_simd |> 
+  pivot_longer(!hbres:simd, names_to = "fin_year", values_to = "value") |> 
+  mutate(group = fin_year, .after = fin_year) |> 
+  mutate(kpi = if_else(str_detect(fin_year, "_add_"), "KPI 1.1 Scotland SIMD Sept coverage", 
+                       "KPI 1.1 Scotland SIMD"), .after = hbres) |> 
+  mutate(fin_year = case_when(str_detect(fin_year, "_year1") ~ year1,
+                              str_detect(fin_year, "_year2") ~ year2),
+         group = case_when(str_detect(group, "cohort") ~ "cohort_n",
+                           str_detect(group, "offer") ~ "offer_n",
+                           str_detect(group, "coverage") ~ "coverage_p")) |> 
+  relocate(simd, .after = fin_year) |>
+  glimpse()
+
+kpi_1_1_simd <- hb_tibble |> left_join(kpi_1_1_simd, by = "hbres")
 
 ## KPI 1.2a ----
 kpi_1_2a <- invite_uptake  |> 
@@ -566,7 +620,7 @@ rm(kpi_1_3a_scot, kpi_1_3b_scot, hb_tibble, simd_level)
 
 
 ## Join summaries ----
-kpi_summary <- rbind(kpi_1_1, kpi_1_2a, kpi_1_2b, kpi_1_3a,  
+kpi_summary <- rbind(kpi_1_1, kpi_1_1_simd, kpi_1_2a, kpi_1_2b, kpi_1_3a,  
                      kpi_1_3a_hb, kpi_1_3b, kpi_1_3b_hb) |> 
   mutate(value = janitor::round_half_up(value, 1))
 
@@ -575,7 +629,7 @@ table(kpi_summary$kpi, kpi_summary$fin_year)
 # Change NaNs to NAs
 kpi_summary$value[is.nan(kpi_summary$value)] <- NA
 
-rm(kpi_1_1, kpi_1_2a, kpi_1_2b, kpi_1_3a, kpi_1_3a_hb, kpi_1_3b, kpi_1_3b_hb)
+rm(kpi_1_1, kpi_1_1_simd, kpi_1_2a, kpi_1_2b, kpi_1_3a, kpi_1_3a_hb, kpi_1_3b, kpi_1_3b_hb)
 
 
 ### Step 6: Add historical data ----
